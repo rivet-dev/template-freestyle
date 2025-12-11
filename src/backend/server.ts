988dacc2e047e9830f2065420e07afece4fc57f9
Nathan Flurry <developer@nathanflurry.com>
2025-12-10 16:57:05 -0800

    changes

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -3,7 +3,7 @@ import { registry } from "./registry";
 registry.start({
 	// https://docs.freestyle.sh/dev-servers/configuration#ports
 	defaultServerPort: 8081,
-	inspector: {
-		token: () => "freestyle",
-	},
+	// inspector: {
+	// 	token: () => "freestyle",
+	// },
 });
